fb0ff87cbf64f6c3d8cd62de2b9488b69c026449
Ken Ho <ken.ho@hotmail.co.uk>
2016-02-02 13:51:31 +0000

    Use test factory, allow username registrations and login, update controller tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,34 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+
+  def self.find_for_database_authentication(warden_conditions)
+   conditions = warden_conditions.dup
+   if login = conditions.delete(:login)
+     where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+   else
+     where(conditions.to_hash).first
+   end
+  end
+
+  validates :username,
+   :presence => true,
+   :uniqueness => {
+     :case_sensitive => false
+    }
+
+  # Only allow letter, number, underscore and punctuation.
+  validate :validate_username
+
+  def validate_username
+   if User.where(email: username).exists?
+     errors.add(:username, :invalid)
+   end
+  end
+
+  def enrolled_in?(course)
+   return enrolled_courses.include?(course)
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   describe "Action: games#index" do
     it "should successfully show the index page" do
+      user = FactoryGirl.create(:user)
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "should require sign in" do
+      get :index
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end
