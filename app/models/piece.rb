020b71ae02c252064a324beb0922650222979891
Michael Kulinski <me@michaelkulinski.com>
2016-02-14 03:20:00 +0000

    Each piece is now associated with a user

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,5 @@ class Piece < ActiveRecord::Base
 	# shared functionality for all pieces goes here
   belongs_to :game
   # Have the game keep track of which user a piece belongs to, instead of directly associating the pieces with a user.
+  belongs_to :player, class_name: "User", foreign_key: :player_id
 end
